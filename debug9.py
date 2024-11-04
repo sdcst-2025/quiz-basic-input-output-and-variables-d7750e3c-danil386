5fa2a53dce63795af912fb1351287961462bc262
danil386 <danielal386@deltalearns.ca>
2024-11-04 11:20:23 -0800

    completed

--- debug9.py
+++ debug9.py
@@ -3,6 +3,8 @@
 Debug this program so that it runs
 """
 x = input("Enter a number")
+x = int(x)
 y = 2
-answer = x+y
-print(The answer is + answer)
+answer = x + y
+answer = str(answer)
+print("The answer is " + answer)
